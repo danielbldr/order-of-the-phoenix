817229a5da4d8d9670c17088a98fbc2cbd51b2ad
Daniel Atkinson <atkinson.daniel7@gmail.com>
2020-05-19 10:35:29 -0600

    Update index action for SearchController and view

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,4 +1,18 @@
 class SearchController < ApplicationController
   def index
+    @house = params['house']
+    conn = Faraday.new(url: 'https://www.potterapi.com/v1/') do |f|
+      f.params['key'] = ENV['HARRY_POTTER_API']
+    end
+
+    response = conn.get('characters') do |f|
+      f.params['orderOfThePhoenix'] = true
+      f.params['house'] = @house
+    end
+
+    json = JSON.parse(response.body, symbolize_names: true)
+    @characters = json.map do |character|
+      Character.new(character)
+    end
   end
 end
