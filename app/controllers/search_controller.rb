a97792bd22a4fceb4880ac62ecedd5fd1a91c4a0
Daniel Atkinson <atkinson.daniel7@gmail.com>
2020-05-19 10:55:57 -0600

    Refactor search controller index action into services and results

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,18 +1,7 @@
 class SearchController < ApplicationController
   def index
+    results = HogwartsResults.new
+    @characters = results.get_pheonix_characters_by_house(@house)
     @house = params['house']
-    conn = Faraday.new(url: 'https://www.potterapi.com/v1/') do |f|
-      f.params['key'] = ENV['HARRY_POTTER_API']
-    end
-
-    response = conn.get('characters') do |f|
-      f.params['orderOfThePhoenix'] = true
-      f.params['house'] = @house
-    end
-
-    json = JSON.parse(response.body, symbolize_names: true)
-    @characters = json.map do |character|
-      Character.new(character)
-    end
   end
 end
